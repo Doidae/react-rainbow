2485d74bf8299223d4aa602c1f3bb85f325e486d
Doidae <andyarredondo1224@gmail.com>
2024-02-17 15:33:58 -0500

    Uploaded files

---
+++ src/ColorBlock.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ColorBlock(props){
+    return (
+        <div className="colorBlock"
+        style={{'backgroundColor': props.color}}>
+            <p>{props.color}</p>
+        </div>
+    )
+}
+
+
+
+//Export
+export default ColorBlock
